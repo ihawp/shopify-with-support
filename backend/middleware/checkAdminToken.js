6ce580af4fa7d8d7615a22924440c6e7737aea9b
ihawp <ihawpebay@gmail.com>
2025-05-06 01:15:10 -0600

    reduce overcomplicated checkAdminToken.js

--- backend/middleware/checkAdminToken.js
+++ backend/middleware/checkAdminToken.js
@@ -5,24 +5,17 @@ const checkAdminToken = (req, res, next) => {
 
   if (!token) return next();
 
-  // should check for token expired, or add another middleware that will check token expired...
-  // all these jwt.verifies are getting expensive.
-
-  try {
-    const decoded = jwt.decode(token);
-    if (decoded?.role === 'admin') {
-      jwt.verify(token, 'admin-secret-token', (err, decoded) => {
-
-        console.log(err);
-
-        if (decoded.role === 'admin') return res.json({ message: 'is-admin' });
-      });
+  jwt.verify(token, 'admin-secret-token', (err, decoded) => {
+    if (err) {
+      return next();
     }
-  } catch (err) {
-    // Fall through
-  }
 
-  next();
-}
+    if (decoded?.role === 'admin') {
+      return res.json({ message: 'is-admin' });
+    }
 
-module.exports = checkAdminToken
+    next();
+  });
+};
+
+module.exports = checkAdminToken;
