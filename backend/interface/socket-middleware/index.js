1f206ed36bc10f8790a91f4e2eaf169ec156592a
ihawp <ihawpebay@gmail.com>
2025-05-09 07:52:09 -0600

    did not need have that admin logic, also had to restructure pulling data from the unread_messages db query.

--- backend/interface/socket-middleware/index.js
+++ backend/interface/socket-middleware/index.js
@@ -12,43 +12,17 @@ module.exports = async ({ socket, io, dbQuery, UserDirector, AdminDirector }) =>
 
     socket.join(roomRef.current);
 
+    // When a guest connects it will fetch their current unread_messages count from support messages so that it is available for admin viewing immediatley.
+    // The value is added to the UserDirector object so that it can be retrieved upon admin join.
     if (role === 'guest') {
-        const unreadData = await dbQuery(`
-            SELECT room, COUNT(*) AS unread 
-            FROM \`support-messages\` 
-            WHERE \`is_read\` = 0 AND \`room\` = ? AND user != "admin"
-        `, [roomRef.current]);
+        const unreadData = await dbQuery(`SELECT room, COUNT(*) AS unread FROM \`support-messages\` WHERE \`is_read\` = 0 AND \`room\` = ? AND user != "admin"`, [roomRef.current]);
 
-        if (!unreadData) {
-            return socket.emit('error', { type: 'db-error', message: 'Error retrieving unread messages.' });
-        }
+        if (!unreadData) return socket.emit('error', { type: 'db-error', message: 'Error retrieving unread messages.' });
 
-        const { unread } = [unreadData];
-        UserDirector.addUser(name, { room: roomRef.current, role, unread });
+        UserDirector.addUser(name, { room: roomRef.current, role, unread: unreadData[0].unread });
     }
 
-    if (role === 'admin') {
-        AdminDirector.addUser(name, { room: roomRef.current, role });
-
-        const unreadMessages = await dbQuery(`
-            SELECT room, COUNT(*) AS unread 
-            FROM \`support-messages\` 
-            WHERE \`is_read\` = 0 AND user != "admin" 
-            GROUP BY room
-        `);
-
-        if (!unreadMessages) {
-            return socket.emit('error', { type: 'db-error', message: 'Error retrieving unread messages.' });
-        }
-
-        const updatedUsers = UserDirector.getAllUsers().map(([socketId, user]) => {
-            const match = unreadMessages.find(msg => msg.room === user.room);
-            return [socketId, { ...user, unread: match?.unread || 0 }];
-        });
-
-        io.to('admin').emit('update-users', updatedUsers);
-    }
-
+    // Get all users current message room/counts and send to all admins (via admin room)
     // Emit updated user list to all clients
     const allUsers = UserDirector.getAllUsers();
     io.to('admin').emit('update-users', allUsers);
@@ -59,14 +33,10 @@ module.exports = async ({ socket, io, dbQuery, UserDirector, AdminDirector }) =>
 
     // Send past messages to the newly joined user
     const pastMessages = await dbQuery('SELECT * FROM `support-messages` WHERE room = ?', [roomRef.current]);
-    if (pastMessages) {
-        socket.emit('past-messages', { messages: pastMessages });
-    }
+    if (pastMessages) socket.emit('past-messages', { messages: pastMessages });
 
-    if (role !== 'admin') {
-        rateLimitAllEvents(socket);
-    }
-
+    // Socket IO Events
+    if (role !== 'admin') rateLimitAllEvents(socket);
     onDisconnect({ socket, io, UserDirector, AdminDirector, role, name });
     onMessage({ socket, io, dbQuery, roomRef, role, timestamp, AdminDirector });
     onChangeRoom({ socket, io, dbQuery, roomRef, role, AdminDirector, name });
